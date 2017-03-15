a6f02a74fbe90cfe5df6f2c9793dbe5e4f64859f
Vishal <vishal.kudale12@gmail.com>
2017-03-14 20:58:58 -0400

    Changes after first review

--- cmds/fsdb.c
+++ cmds/fsdb.c
@@ -14,17 +14,22 @@
 struct mfs_super_block sb;
 struct mfs_inode_map imap;
 struct mfs_block_map bmap;
+char buf[MFS_BLOCKSIZE];
 int devfd;
 
+/*
+ *  Read the inode block
+ */
 int read_inode(int block_no, int offset, struct mfs_inode *uip)
 {	
-//	printf("imap.map[2] : %d \n",imap.map[2]);
-
 	lseek(devfd,((block_no*MFS_BLOCKSIZE)+offset),SEEK_SET);
 	read(devfd, (char *)uip, sizeof(struct mfs_inode));
 	return 0;
 }
 
+/*
+ * Read the SuperBlock
+ */
 int read_sbnode(int block_no, int offset,struct mfs_super_block *usp)
 {
 	lseek(devfd,((block_no*MFS_BLOCKSIZE)+offset),SEEK_SET);
@@ -32,135 +37,144 @@ int read_sbnode(int block_no, int offset,struct mfs_super_block *usp)
 	return 0;
 }
 
+/*
+ * Print the SuperBlock details
+ */
 void print_sbnode(struct mfs_super_block *usp)
 {
-	printf("msb_magic                :0x%x\n",usp->msb_magic);
-        printf("msb_n_free_inode         : %d \n",usp->msb_n_free_inode);
-        printf("msb_n_free_blks          : %d \n",usp->msb_n_free_blks);
-
+	printf("msb_magic\t\t:0x%x\n",usp->msb_magic);
+        printf("msb_n_free_inode\t: %d \n",usp->msb_n_free_inode);
+        printf("msb_n_free_blks\t\t: %d \n",usp->msb_n_free_blks);
 }
 
+/*
+ * Print the inode details 
+ */
 void print_inode(struct mfs_inode *uip)
 {
-	char buf[MFS_BLOCKSIZE];
-	struct mfs_directory_entry *dirent;
-	int i,x;
+	int i;
 
-
-//	printf("\nInode numner : %d \n",inum);
-	printf("mi_mode            : %x\n",uip->mi_mode);
-	printf("mi_uid             : %d\n",uip->mi_uid);
-	printf("mi_gid             : %d\n",uip->mi_gid);
-	printf("mi_atime           : %s\n",ctime((time_t *)&uip->mi_atime));
-	printf("mi_ctime           : %s\n",ctime((time_t *)&uip->mi_ctime));
-	printf("mi_mtime           : %s\n",ctime((time_t *)&uip->mi_mtime));
-	printf("mi_links_count     : %d\n",uip->mi_links_count);
-	printf("mi_size            : %d\n",uip->mi_size);
-	printf("mi_blocks          : %d\n",uip->mi_blocks);
+	printf("mi_mode\t\t: %x\n",uip->mi_mode);
+	printf("mi_uid\t\t: %d\n",uip->mi_uid);
+	printf("mi_gid\t\t: %d\n",uip->mi_gid);
+	printf("mi_atime\t: %s\n",ctime((time_t *)&uip->mi_atime));
+	printf("mi_ctime\t: %s\n",ctime((time_t *)&uip->mi_ctime));
+	printf("mi_mtime\t: %s",ctime((time_t *)&uip->mi_mtime));
+	printf("mi_links_count\t: %d\n",uip->mi_links_count);
+	printf("mi_size\t\t: %d\n",uip->mi_size);
+	printf("mi_blocks\t: %d\n",uip->mi_blocks);
 	
-	for(i=0;i<MFS_IBLOCK_COUNT;i++)
-	{
-		if(i%4==0)
-			printf("\n");
-		printf("mi_blk_add[%2d]:%3d\t",i,uip->mi_blk_add[i]);
+	printf("Block Number\t:");
+	for (i=0;i<MFS_IBLOCK_COUNT;i++) {
+		printf("%2d\t",i);
+	}
+
+	printf("\n");
+	printf("Adress\t\t:");
+	for (i=0;i<MFS_IBLOCK_COUNT;i++) {
+		printf("%3d\t",uip->mi_blk_add[i]);
 	}
 	printf("\n");
-
-	/* Print out the directory entries */
-
-	if(uip->mi_mode & S_IFDIR)
-	{
-		printf("\n\n Directory entries : \n");
-		printf("\tInode_number\tDirectory_name\n\n");
-		
-		for(i=0;i<uip->mi_blocks;i++)
-		{
-			lseek(devfd,uip->mi_blk_add[i]*MFS_BLOCKSIZE,SEEK_SET);
-			read(devfd,buf,MFS_BLOCKSIZE);
-			dirent = (struct mfs_directory_entry *)buf;
-			for(x=0;x<MFS_IBLOCK_COUNT;x++)
-			{
-				if(dirent->inode_num!=0)
-				{
-					printf("%d\t%2d\t\t%s\n",x,dirent->inode_num,dirent->name);
-				}
-				dirent++;
-			}
-
-		}
-	printf("\n");	
-		
-	}
-	else
-	{
-		printf("\n\n");
-	}
 }
 
+/*
+ *  Read the directory entry
+*/
+int read_dir(int block_no,int offset,char *buf)
+{
+	lseek(devfd,((block_no*MFS_BLOCKSIZE)+offset),SEEK_SET);
+	read(devfd,buf,MFS_BLOCKSIZE);
+	return 0;
+}
+
+/*
+ * Print out the directory entry
+ */
+void print_dir(char *buf)
+{
+	int x;
+	struct mfs_directory_entry *dirent;
+
+	dirent=(struct mfs_directory_entry *)buf;
+
+	printf("\n\nDirectory entries : \n\n");
+        printf("Sr.No\tInode_number\tDirectory_name\n\n");
+
+	for (x=0;x<MFS_IBLOCK_COUNT;x++) {
+        	if (dirent->inode_num!=0) {
+            		printf("%d\t%2d\t\t%s\n",x,dirent->inode_num,dirent->name);
+          	}
+          	dirent++;
+        }
+}
+
+/*
+ * To disply the usage of the fsdb command
+ */
+void usage()
+{
+	printf("\nUsage:\n");
+	printf("\n ./fsdb.mfs <device_name>  <block_number>  <off_set>  <structure_name>\n");
+	printf("\nDebugging Utility for Micro file system.\n");
+
+	printf("\nOptions :\n");
+	printf("<device_name>\t\tpath to the device to be used.\n");
+	printf("<block_number>\t\tBlock number of the structure.\n");
+	printf("<offset>\t\tOffset value in the block.\n");
+	printf("<structure_name>\tStructure name whose details needs to be printed.\n");
+	printf("\t\t\t1.superblock 2.inode 3.directory 4.inodemap 5.blockmap\n");
+        exit(1);
+
+}
 
 int main(int argc,char **argv)
 {
 	struct mfs_inode inode;
 	int block_no,offset,i;
 
-	if(argc !=5)
-	{
-		printf("\n mfsdb.mfs <device_name  block_number  off_set  structure_name>");
-		exit(1);
+	if (argc !=5) {
+		usage();
 	}
 
 	devfd = open(argv[1], O_RDWR);
 
-	if(devfd < 0)
-	{
+	if (devfd < 0) {
 		printf("Failed to open the device \n");
 	}
 
-//	read(devfd, (char *)&sb, sizeof(struct mfs_super_block));
-
-/*	if(sb.msb_magic != MFS_MAGIC)
-	{
-		printf("This is not the mfs filesystem \n");
-		exit(1);
-	}
-*/
 	block_no=atoi(argv[2]);
 	offset=atoi(argv[3]);
 
-	if(strcmp(argv[4],"inode")==0)
-	{
+	if (strcmp(argv[4],"inode")==0) {
 		read_inode(block_no,offset,&inode);
 		print_inode(&inode);
 	}
 
-	if(strcmp(argv[4],"superblock")==0)
-	{
+	if (strcmp(argv[4],"superblock")==0) {
 		read_sbnode(block_no,offset,&sb);
 		print_sbnode(&sb);
 	}
-	if(strcmp(argv[4],"inodemap")==0)
-	{
+
+	if (strcmp(argv[4],"inodemap")==0) {
 		lseek(devfd,((block_no*MFS_BLOCKSIZE)+offset),SEEK_SET);
 		read(devfd,(char *)&imap,sizeof(struct mfs_inode_map));
 		printf("%d",imap.map[0]);
-		for(i=1;i<1024;i++)
-		{
-			if(i%8==0)
+		for (i=1;i<1024;i++) {
+			if (i%8==0)
 				printf(" ");
-			if(i%64==0)
+			if (i%64==0)
 				printf("\n");
 			printf("%d",imap.map[i]);
 			
 		}
 		printf("\n");
 	}
-	if(strcmp(argv[4],"blockmap")==0)
-	{
+
+	if (strcmp(argv[4],"blockmap")==0) {
 		lseek(devfd,((block_no*MFS_BLOCKSIZE)+offset),SEEK_SET);
                 read(devfd,(char *)&bmap,sizeof(struct mfs_block_map));
                 printf("%d",bmap.map[0]);
-                for(i=1;i<1024;i++)
-                {
+                for (i=1;i<1024;i++) {
                         if(i%8==0)
                                 printf(" ");
                         if(i%64==0)
@@ -170,7 +184,16 @@ int main(int argc,char **argv)
                 }
 		printf("\n");
 	}
-	
-		
+
+	if (strcmp(argv[4],"directory")==0) {
+		read_dir(block_no,offset,buf);
+		print_dir(buf);
+	}
+	else
+	{
+		printf("\nInvalid Command\n");
+		usage();
+	}
+
 	return 0;
 }
